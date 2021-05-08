626870489c63566f7dc57f01436d61d1b87c0672
orangina1018 <orangina1018aaa@gmail.com>
2021-05-08 16:18:18 +0900

    add never & null & object & void

---
+++ src/object.ts
@@ -0,0 +1,10 @@
+export {};
+
+let profile1: object = { name: 'Ham'};
+profile1 = { birthday: 1994};
+
+
+let profile2 : {
+  name: string;
+} = { name : 'ham'};
+profile2 = { birthday: 1994};
---
+++ src/void.ts
@@ -0,0 +1,7 @@
+export {};
+
+function returnnothing() : void {
+  console.log("i dont return nothing");
+}
+
+console.log(returnnothing());
